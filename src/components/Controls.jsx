45cda96a8817f37bebb78e7963317ca7f13f2ae5
rahul <rahulbeniwal18112002@gmail.com>
2025-02-27 15:46:41 +0530

    updated favicon

--- src/components/Controls.jsx
+++ src/components/Controls.jsx
@@ -5,7 +5,7 @@ import Share from './Share';
 import UpdateLink from './UpdateLink';
 import ManageLinks from './ManageLinks';
 
-const isLoggedIn = !!localStorage.getItem('access_token');
+const isLoggedIn = localStorage.getItem('access_token');
 
 
 export default function Controls({ isDarkTheme, setIsDarkTheme, leftContent, rightContent, selectedLanguage, setShowUpdateButton, showUpdateButton}) {
@@ -14,7 +14,7 @@ export default function Controls({ isDarkTheme, setIsDarkTheme, leftContent, rig
 
     return (
         <div className="controls flex items-center gap-3 text-gray-200">
-            {diffId &&  showUpdateButton (
+            {diffId &&  showUpdateButton && (
                 <UpdateLink 
                     leftContent={leftContent} 
                     rightContent={rightContent} 
